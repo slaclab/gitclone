17193ed6e5d2e49b39891a5864f67c26ca4ce28c
yee379 <yee379@gmail.com>
2023-09-20 15:56:07 -0700

    fix: git permissions

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,8 @@ FROM alpine:latest
 
 RUN apk add git
 RUN mkdir /app
+RUN ln -sf /tmp/.gitconfig /.gitconfig
 COPY entrypoint-gitclone.sh /entrypoint-gitclone.sh
 
-ENTRYPOINT ["sh" "/entrypoint-gitclone.sh"]
+ENTRYPOINT ["sh", "/entrypoint-gitclone.sh"]
 
--- entrypoint-gitclone.sh
+++ entrypoint-gitclone.sh
@@ -16,18 +16,25 @@ GIT_CLONE_ONLY=${GIT_CLONE_ONLY:-0}
 # always ensure there's some documentation
 mkdir -p ${GIT_PATH}
 cd ${GIT_PATH}
+
+# enforce
+git config --global --add safe.directory ${GIT_PATH}
+
+# checkout
 git status
 STATUS=$?
+echo "Git status of ${GIT_PATH}: ${STATUS}"
 if [ $STATUS -gt 0 ]; then
-    git clone ${GIT_REPO} .
+  echo "Cloning ${GIT_REPO}:${GIT_RELEASE} to ${GIT_PATH}..."
+  git clone ${GIT_REPO} .
 fi
 
 # do some funky stuff with git remove here
-
+echo "Checking out ${GIT_RELEASE}..."
 git checkout ${GIT_RELEASE}
 git fetch origin
 
-if [ ! $GIT_CLONE_ONLY ]; then
+if [[ ${GIT_CLONE_ONLY} -eq 0 ]]; then
 while :
 do
   echo "$(date '+%Y-%m-%d %H%M.%S %z%Z') Updating git repo ${GIT_REPO}:${GIT_RELEASE}"
